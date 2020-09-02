6ff4c1f6c2bb4619cffb819e9b3c071363f0453c
eim5178 <eim5178@psu.edu>
2020-09-02 21:30:28 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+// Evelyn Moore eim5178@psu.edu
+// Collaborators
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  char *celsius = (readline("Enter temperature in celsius: " ));
+  double temp = atof(celsius);
+  double fahrenheit_temp;
+  fahrenheit_temp = (temp * 9/5) + 32;
+  printf("%f°in Celsius is equivalent to %f Fahrenheit.\f", temp, fahrenheit_temp);
+}
